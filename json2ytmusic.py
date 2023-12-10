9bf68db9338f40be99310f849a63a95a1f698604
Davide Depau <davide@depau.eu>
2023-12-10 18:35:55 +0100

    Allow caching YTMusic queries

--- json2ytmusic.py
+++ json2ytmusic.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 import argparse
 import json
+import os
 
 from ytmusicapi import YTMusic
+
+YELLOW = '\033[33m'
+GREEN = '\033[32m'
+RESET = '\033[0m'
 
 
 def main():
@@ -11,9 +16,16 @@ def main():
     parser.add_argument('playlist_id', help='YouTube Music playlist ID')
     parser.add_argument('input', nargs='?', type=argparse.FileType('r'), default='-',
                         help='Apple Music playlist JSON file (default stdin)')
+    parser.add_argument('-o', '--oauth', default='oauth.json', help='OAuth file (default oauth.json)')
+    parser.add_argument('-c', '--cache', default='ytmusic-cache.json', help='Cache file (default ytmusic-cache.json)')
     args = parser.parse_args()
 
-    yt = YTMusic("oauth.json")
+    yt = YTMusic(args.oauth)
+
+    cache = {}
+    if os.path.exists(args.cache):
+        with open(args.cache, 'r') as f:
+            cache = json.load(f)
 
     # noinspection PyTypeChecker
     dst_playlist = yt.get_playlist(args.playlist_id, limit=None)
@@ -27,32 +39,44 @@ def main():
 
     to_add = []
 
-    for song in src_playlist:
-        title = song['title']
-        artist = song['artist']
+    try:
+        for song in src_playlist:
+            title = song['title']
+            artist = song['artist']
 
-        query = f'{title} {artist}'
+            query = f'{title} {artist}'
 
-        results = yt.search(query, filter='songs', limit=5)
-        if len(results) == 0:
-            print(f'\033[31m No results for {query}\033[0m')
-            continue
+            if query in cache and cache[query] not in existing and cache[query] not in to_add:
+                # print the line in yellow
+                print(f'{YELLOW} {title} - {artist} '.ljust(60, ' '), end='')
+                print(f'-> cached: {cache[query]}{RESET}')
+                to_add.append(cache[query])
+                continue
 
-        # YouTube search is kinda good, so just take the first result
-        best_result = results[0]
+            results = yt.search(query, filter='songs', limit=5)
+            if len(results) == 0:
+                print(f'\033[31m No results for {query}{RESET}')
+                continue
 
-        print(f'\033[32m {title} - {artist} '.ljust(60, ' '), end='')
-        print(f'-> {best_result["title"]} - {best_result["artists"][0]["name"]}\033[0m')
+            # YouTube search is kinda good, so just take the first result
+            best_result = results[0]
 
-        if best_result['videoId'] not in existing and best_result['videoId'] not in to_add:
-            to_add.append(best_result['videoId'])
+            print(f'{GREEN} {title} - {artist} '.ljust(60, ' '), end='')
+            print(f'-> {best_result["title"]} - {best_result["artists"][0]["name"]}{RESET}')
 
-    print(f'\033[33m Adding {len(to_add)} songs...\033[0m')
+            if best_result['videoId'] not in existing and best_result['videoId'] not in to_add:
+                to_add.append(best_result['videoId'])
+                cache[query] = best_result['videoId']
+    finally:
+        with open(args.cache, 'w') as f:
+            json.dump(cache, f, indent=4)
 
-    print(json.dumps(to_add))
-    yt.add_playlist_items(args.playlist_id, videoIds=to_add)
+    print(f'{YELLOW} Adding {len(to_add)} songs...{RESET}')
 
-    print('\033[32m Done!\033[0m')
+    if len(to_add) > 0:
+        yt.add_playlist_items(args.playlist_id, videoIds=to_add)
+
+    print(f'{GREEN} Done!{RESET}')
 
 
 if __name__ == '__main__':
